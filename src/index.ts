cc57fa827812efb71ac93026f9b0911e15c7c890
CaioChristhian <caiochristhianz@gmail.com>
2023-10-16 16:55:10 -0300

    finalizando rotas da api de categories, products e orders

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
+import path from 'node:path';
+
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
 
 const app = express();
 const port = 3001;
 
 mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
+
+		app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+		app.use(express.json());
+		app.use(router);
 
 		app.listen(port, () => {
 			console.log(`Server is running on http://localhost:${port}`);
